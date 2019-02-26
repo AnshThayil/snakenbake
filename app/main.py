27bde43157dc79563bb2e3a6bed693bbc0e0ef08
Ansh Thayil <anshthayil0114@gmail.com>
2019-02-26 03:09:41 -0800

    Avoids walls and own body while collecting food

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import json
 import os
-import random
 import bottle
 
 from api import ping_response, start_response, move_response, end_response
@@ -39,9 +38,9 @@ def start():
             initialize your snake state here using the
             request's data if necessary.
     """
-    print(json.dumps(data))
+    #print(json.dumps(data))
 
-    color = "#00FF00"
+    color = "#93abe1"
 
     return start_response(color)
 
@@ -54,10 +53,18 @@ def move():
     TODO: Using the data from the endpoint request object, your
             snake AI must choose a direction to move in.
     """
-    print(json.dumps(data))
+    #print(json.dumps(data))
+    body = data["you"]["body"]
+    head = body[0]
+    board = data["board"]
+    food_locations = data["board"]["food"]
+    nearest_food = calculate_nearest_food(food_locations, head)
+    direction = generate_next_move(nearest_food, body)
+    print(data["turn"])
+    #print(direction)
+    direction = correct_path(direction, body, board)
+    #print(direction)
 
-    directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
 
     return move_response(direction)
 
@@ -76,6 +83,78 @@ def end():
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
+
+def calculate_nearest_food(food_locations, head):
+    min = abs(food_locations[0]["x"] - head["x"]) + abs(food_locations[0]["y"] - head["y"])
+    min_index = 0
+    for food in food_locations:
+        dist = abs(food["x"] - head["x"]) + abs(food["y"] - head["y"])
+        if dist <= min:
+            min = dist
+            min_index = food_locations.index(food)
+    return (food_locations[min_index])
+
+def generate_next_move(nearest_food, body):
+    head = body[0]
+    if ((head["x"] == nearest_food["x"]) and (head["y"] != nearest_food["y"])):
+        if (head["y"] > nearest_food["y"]):
+            output = "up"
+        else:
+            output = "down"
+    elif ((head["y"] == nearest_food["y"])and (head["x"] != nearest_food["x"])):
+        if (head["x"] > nearest_food["x"]):
+            output = "left"
+        else:
+            output = "right"
+    else:
+        if abs(head["x"] - nearest_food["x"]) <= abs(head["y"] - nearest_food["y"]):
+            if (head["x"] > nearest_food["x"]):
+                output = "left"
+            else:
+                output = "right"
+        else:
+            if (head["y"] > nearest_food["y"]):
+                output = "up"
+            else:
+                output = "down"
+    return output
+
+def correct_path(direction, body, board):
+    head = body[0]
+    enemies = board["snakes"]
+    if direction == "up":
+        next_place = {"x": head["x"], "y": head["y"] - 1}
+        if next_place in body or next_place["y"] == -1:
+            direction = "right"
+            print(direction)
+            return correct_path(direction, body, board)
+        else:
+            return direction
+    elif direction == "down":
+        next_place = {"x": head["x"], "y": head["y"] + 1}
+        if next_place in body or next_place["y"] == board["height"]:
+            direction = "left"
+            print(direction)
+            return correct_path(direction, body, board)
+        else:
+
+            return direction
+    elif direction == "right":
+        next_place = {"x": head["x"] + 1, "y": head["y"]}
+        if next_place in body or next_place["x"] == board["width"]:
+            direction = "down"
+            print(direction)
+            return correct_path(direction, body, board)
+        else:
+            return direction
+    elif direction == "left":
+        next_place = {"x": head["x"] - 1, "y": head["y"]}
+        if next_place in body or next_place["x"] == -1:
+            direction = "up"
+            print(direction)
+            return correct_path(direction, body, board)
+        else:
+            return direction
 
 if __name__ == '__main__':
     bottle.run(

3b749d7f6ca6a1c2f12badeafcd4a8561f64f10d
Ansh Thayil <anshthayil0114@gmail.com>
2019-03-06 20:20:19 -0800

    Init

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 import bottle
 
 from api import ping_response, start_response, move_response, end_response
@@ -16,7 +17,6 @@ def static(path):
     """
     Given a path, return the static file located relative
     to the static folder.
-
     This can be used to return the snake head URL in an API response.
     """
     return bottle.static_file(path, root='static/')
@@ -38,9 +38,9 @@ def start():
             initialize your snake state here using the
             request's data if necessary.
     """
-    #print(json.dumps(data))
+    print(json.dumps(data))
 
-    color = "#93abe1"
+    color = "#00FF00"
 
     return start_response(color)
 
@@ -53,37 +53,11 @@ def move():
     TODO: Using the data from the endpoint request object, your
             snake AI must choose a direction to move in.
     """
-    #print(json.dumps(data))
-    body = data["you"]["body"]
-    myName = data["you"]["name"]
-    head = body[0]
-    board = data["board"]
-    food_locations = data["board"]["food"]
-    health = data["you"]["health"]
-    x = 5
-    y = 15
-    if len(food_locations) == 0:
-        nearest_food = body[len(body) -1]
-    else:
-        nearest_food = calculate_nearest_food(food_locations, head)
-    direction = generate_next_move(nearest_food, body)
-    print(data["turn"])
-    #print(direction)
-    count = 0
-    direction = correct_path(direction, body, board, nearest_food, count, myName)
-    #print(direction)
+    print(json.dumps(data))
 
-    # if (len(food_locations) == 0):
-    #     direction = coil()
-    # else:
-    #     if (len(snake) >= x):
-    #         if (health < distance_to_nearest_food + y):
-    #             direction = coil()
-    #         else:
-    #             direction = move_to_food()
-    #     else:
-    #         direction = move_to_food()
-    #
+    directions = ['up', 'down', 'left', 'right']
+    direction = random.choice(directions)
+
     return move_response(direction)
 
 
@@ -101,119 +75,6 @@ def end():
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
-
-def calculate_nearest_food(food_locations, head):
-    min = abs(food_locations[0]["x"] - head["x"]) + abs(food_locations[0]["y"] - head["y"])
-    min_index = 0
-    for food in food_locations:
-        dist = abs(food["x"] - head["x"]) + abs(food["y"] - head["y"])
-        if dist <= min:
-            min = dist
-            min_index = food_locations.index(food)
-    return (food_locations[min_index])
-
-def generate_next_move(nearest_food, body):
-    head = body[0]
-    if ((head["x"] == nearest_food["x"]) and (head["y"] != nearest_food["y"])):
-        if (head["y"] > nearest_food["y"]):
-            output = "up"
-        else:
-            output = "down"
-    elif ((head["y"] == nearest_food["y"])and (head["x"] != nearest_food["x"])):
-        if (head["x"] > nearest_food["x"]):
-            output = "left"
-        else:
-            output = "right"
-    else:
-        if abs(head["x"] - nearest_food["x"]) <= abs(head["y"] - nearest_food["y"]):
-            if (head["x"] > nearest_food["x"]):
-                output = "left"
-            else:
-                output = "right"
-        else:
-            if (head["y"] > nearest_food["y"]):
-                output = "up"
-            else:
-                output = "down"
-    return output
-
-def correct_path(direction, body, board,nearest_food, count, myName):
-    print(count)
-    if count < 4:
-        head = body[0]
-        enemy_coords = []
-        enemy_heads = []
-        enemies = board["snakes"]
-        for enemy in enemies:
-
-            if enemy["name"] != myName:
-                if enemy["body"][0] not in board["food"]:
-                    for i in range(len(enemy["body"]) - 1):
-                        if i == 0:
-                            enemy_heads.append(enemy["body"][i])
-                        enemy_coords.append(enemy["body"][i])
-                else:
-                    for i in range(len(enemy["body"])):
-                        if i == 0:
-                            enemy_heads.append(enemy["body"][i])
-                        enemy_coords.append(enemy["body"][i])
-
-
-
-        if direction == "up":
-            next_place = {"x": head["x"], "y": head["y"] - 1}
-            if next_place in body or next_place["y"] == -1 or next_place in enemy_coords or surround_check(enemy_heads, next_place, nearest_food):
-                direction = "right"
-                count += 1
-                return correct_path(direction, body, board, nearest_food, count, myName)
-            else:
-                return direction
-
-        elif direction == "down":
-            next_place = {"x": head["x"], "y": head["y"] + 1}
-            if next_place in body or next_place["y"] == board["height"] or next_place in enemy_coords or surround_check(enemy_heads, next_place, nearest_food):
-                direction = "left"
-                count += 1
-                return correct_path(direction, body, board, nearest_food, count, myName)
-            else:
-                return direction
-
-        elif direction == "right":
-            next_place = {"x": head["x"] + 1, "y": head["y"]}
-            if next_place in body or next_place["x"] == board["width"] or next_place in enemy_coords or surround_check(enemy_heads, next_place, nearest_food):
-                direction = "down"
-                count += 1
-                return correct_path(direction, body, board, nearest_food, count, myName)
-            else:
-                return direction
-
-        elif direction == "left":
-            next_place = {"x": head["x"] - 1, "y": head["y"]}
-            if next_place in body or next_place["x"] == -1 or next_place in enemy_coords or surround_check(enemy_heads, next_place, nearest_food):
-                direction = "up"
-                count += 1
-                return correct_path(direction, body, board, nearest_food, count, myName)
-            else:
-                return direction
-    else:
-        return direction
-
-def surround_check(enemy_heads, next_place, nearest_food):
-    if next_place == nearest_food:
-        surround = []
-        surround.append({"x": next_place["x"] + 1, "y": next_place["y"]})
-        surround.append({"x": next_place["x"] - 1, "y": next_place["y"]})
-        surround.append({"x": next_place["x"], "y": next_place["y"] + 1})
-        surround.append({"x": next_place["x"], "y": next_place["y"] - 1})
-        for coord in surround:
-            if coord in enemy_heads:
-                return True
-    return False
-
-
-
-
-
 
 if __name__ == '__main__':
     bottle.run(
